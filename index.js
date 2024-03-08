a22285fe951c28d5049e0c8bfb5017672b727d8e
dhanarajappu456 <37978294+dhanarajappu456@users.noreply.github.com>
2024-03-08 14:33:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+//const axios = require("./axios");
+const port = 3000;
+
+app.get("/test", (req, res) => res.send("Test working"));
+
+app.get("/userInfo/:user", (req, res) => {
+  const user = String(req.params.user);
+  const url = "https://leetcode.com/graphql";
+  console.log("data", user);
+  let query = `{
+  allQuestionsCount {
+    difficulty
+    count
+  }
+  matchedUser(username :"${user}") {
+    problemsSolvedBeatsStats {
+      difficulty
+      percentage
+    }
+    submitStatsGlobal {
+      acSubmissionNum {
+        difficulty
+        count
+      }
+    }
+  }
+}`;
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Referer": "https://leetcode.com",
+    },
+    body: JSON.stringify({ query: query, variables: { username: user } }),
+  })
+    .then((result) => result.json())
+    .then((result) => {
+      console.log(JSON.stringify(result));
+      res.status(200).json({
+        data: result.data,
+      });
+    })
+    .catch((err) => console.log("err", err));
+});
+
+// axios.post(url)
+
+app.listen(port, () => console.log(`Express app running on port ${port}!`));
